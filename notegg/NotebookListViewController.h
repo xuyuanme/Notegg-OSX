b1873483ef221c0093439d58bd3d0d8975cc1841
xuyuanme <xuyuanme@gmail.com>
2014-08-06 16:36:51 +0800

    Initial commit

---
+++ notegg/NotebookListViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotebookListViewController.h
+//  notegg
+//
+//  Created by Yuan on 14-8-6.
+//  Copyright (c) 2014年 xuyuanme. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NotebookListViewController : NSViewController
+
+@end
---
+++ notegg/NotebookListViewController.m
@@ -0,0 +1,26 @@
+//
+//  NotebookListViewController.m
+//  notegg
+//
+//  Created by Yuan on 14-8-6.
+//  Copyright (c) 2014年 xuyuanme. All rights reserved.
+//
+
+#import "NotebookListViewController.h"
+
+@interface NotebookListViewController ()
+
+@end
+
+@implementation NotebookListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+@end
